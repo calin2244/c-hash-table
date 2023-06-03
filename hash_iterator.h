a9a95324cee55cd7b8712d7903535a158a90eff1
calin2244 <bastureacalin@gmail.com>
2023-06-03 19:30:05 +0300

    Fixed some bugs with traversing

--- hash_iterator.h
+++ hash_iterator.h
@@ -14,6 +14,7 @@ HashIterator ht_iterator_create(HashTable* ht){
     return hiter;
 }
 
+// practically places the cursor / curr_bucket at the next available bucket if there is one
 bool ht_iterator_has_next(HashIterator* iterator){
     while(iterator->curr_bucket < iterator->ht->capacity){
         if(iterator->curr_item)
@@ -30,15 +31,14 @@ Ht_Item* ht_iterator_get(HashIterator* iterator){
     Ht_Item* item = iterator->curr_item;
     if(item)
         return item;
-    else{
+    
+    iterator->curr_bucket++;
+    while(iterator->curr_bucket < iterator->ht->capacity){
+        iterator->curr_item = iterator->ht->buckets[iterator->curr_bucket];
+        if(iterator->curr_item)
+            return iterator->curr_item;
+        
         iterator->curr_bucket++;
-        while(iterator->curr_bucket < iterator->ht->capacity){
-            iterator->curr_item = iterator->ht->buckets[iterator->curr_bucket];
-            if(iterator->curr_item)
-                return iterator->curr_item;
-            
-            iterator->curr_bucket++;
-        }
     }
 
     return NULL;
@@ -62,8 +62,9 @@ Ht_Item* ht_iterator_next(HashIterator* iterator){
 }
 
 Ht_Item* ht_iterator_get_nth_item(HashIterator* iterator, size_t n){    
-    if(n > iterator->ht->size)
-        n %= iterator->ht->size;
+    // out of bounds
+    if(n > iterator->ht->capacity)
+        return NULL;
 
     while(ht_iterator_has_next(iterator) && n){
         Ht_Item* curr = ht_iterator_get(iterator);
