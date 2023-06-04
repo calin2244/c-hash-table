2b8c8fdf70b46791862eb50157da846cab51ed63
calin2244 <bastureacalin@gmail.com>
2023-06-05 00:41:45 +0300

    Added Time Performances

--- htt.c
+++ htt.c
@@ -1,6 +1,6 @@
 #include "hash_iterator.h" // include "hash_map.h" too
 #include "hash_map.h"
-#include "time.h"
+#include <time.h>
 
 void printItemInfo(Ht_Item* item){
     if(item){
@@ -8,7 +8,7 @@ void printItemInfo(Ht_Item* item){
     }
 }
 
-int main(int argc, char* argv[]){
+int main(void){
     HashTable* hash_t = ht_create(CAPACITY, hash_func_str);
     HashIterator iterator = ht_iterator_create(hash_t);
     
@@ -25,16 +25,40 @@ int main(int argc, char* argv[]){
     // Reset the iterator to point at the beginning of the Hash Table
     ht_iterator_reset(&iterator);
 
+    // let's time going all over the Hash Table, even iterating throught the empty buckets
+    clock_t start_time, end_time;
+    start_time = clock();
+
     while(ht_iterator_has_next(&iterator)){
-        Ht_Item* curr = ht_iterator_get(&iterator);
-        printItemInfo(curr);
+        (void)ht_iterator_get(&iterator);
+        // printItemInfo(curr);
         ht_iterator_next(&iterator);
-    }
+    }    
+
+    end_time = clock();
+
+    double cpu_time_used = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+
+    // Time Performances
+    (void)printf("Totale time to traverse Hash Table: %f\n", cpu_time_used);
 
     printHashTableInfo(hash_t);
-
     parseFileAndRemoveEntries(hash_t, ENTRIES);
-    ht_free(&hash_t);
 
+    ht_free(&hash_t);
     return 0;
 }
+
+
+    // Ht_Item* c = hash_t->buckets[9258];
+    // Ht_Item** p = &c;
+
+    // while(*p){
+    //     // printItemInfo(*p);
+    //     p = &(*p)->next;
+    // }
+
+    // while(c){
+    //     printItemInfo(c);
+    //     c = c->next;
+    // }
