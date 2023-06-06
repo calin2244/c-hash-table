42a23232ade929a4db169f61a3a24162b0d9fb7c
calin2244 <bastureacalin@gmail.com>
2023-06-06 22:11:06 +0300

    Main Function

--- htt.c
+++ htt.c
@@ -8,24 +8,19 @@ void printItemInfo(Ht_Item* item){
     }
 }
 
-int main(void){
-    HashTable* hash_t = ht_create(CAPACITY, hash_func_str);
+int main(int argc, char* argv[]){
+    HashTable* hash_t = ht_create(7, hash_func_str);
     HashIterator iterator = ht_iterator_create(hash_t);
     
     parseFileAndPopulateHashTable(hash_t, "entries.txt");
 
     // This will retrieve the 100th item in the Hash Table
-    Ht_Item* onehundreth = ht_iterator_get_nth_item(&iterator, 100);
-    Ht_Item* maxChainNode = hash_t->buckets[maximumChainLength(hash_t).hashOfMaxChain]; 
+    // Ht_Item* onehundreth = ht_iterator_get_nth_item(&iterator, 100);
     
-    // To output the chain, traverse the maxChainNode as a simple linked list
-    printf("Maximum Length Chain Node: ");
-    printItemInfo(maxChainNode);
-
     // Reset the iterator to point at the beginning of the Hash Table
     ht_iterator_reset(&iterator);
 
-    // let's time going all over the Hash Table, even iterating throught the empty buckets
+    // Let's time iterating all over the Hash Table, even iterating through the empty buckets
     clock_t start_time, end_time;
     start_time = clock();
 
@@ -38,11 +33,23 @@ int main(void){
     end_time = clock();
 
     double cpu_time_used = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+    
+    // Show Interesting Stats
+    if(argc > 1 && strcmp(argv[1], "stats") == 0){
+        // To output the chain, traverse the maxChainNode as a simple linked list
+        Ht_Item* maxChainNode = hash_t->buckets[maximumChainLength(hash_t).hashOfMaxChain]; 
+        (void)printf("Maximum Length Chain Node: ");
+        printItemInfo(maxChainNode);
 
-    // Time Performances
-    (void)printf("Totale time to traverse Hash Table: %f\n", cpu_time_used);
+        // Time Performances
+        (void)printf("Total time to traverse Hash Table: %f\n", cpu_time_used);
 
-    printHashTableInfo(hash_t);
+        printHashTableInfo(hash_t);
+    }
+    else if(argc > 1 && strcmp(argv[1], "print") == 0){
+        print_ht(hash_t, print_string_string);
+    }
+    
     parseFileAndRemoveEntries(hash_t, ENTRIES);
 
     ht_free(&hash_t);
