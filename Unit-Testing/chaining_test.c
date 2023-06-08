f28733f9f8a49469e2bcc9fa7ed1ef60f8145f20
calin2244 <bastureacalin@gmail.com>
2023-06-08 23:59:56 +0400

    Finished Testing Environment

--- Unit-Testing/chaining_test.c
+++ Unit-Testing/chaining_test.c
@@ -4,12 +4,7 @@
 #include <time.h>
 
 #define ENTRIES_PATH "./Data(input)/entries.txt"
-
-void printItemInfo(Ht_Item* item){
-    if(item){
-        printf("%s: %s\n", (char*)item->key, (char*)item->val);
-    }
-}
+#define OUTPUT_PATH "./Data(output)/chain_out.txt"
 
 int main(int argc, char* argv[]){
     HashTable* hash_t = ht_create(13, hash_func_str, CHAINING);
@@ -40,24 +35,30 @@ int main(int argc, char* argv[]){
 
     double cpu_time_used = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
     
-    // Show Interesting Stats
-    if(argc > 1 && strcmp(argv[1], "stats") == 0){
-        // To output the chain, traverse the maxChainNode as a simple linked list
-        Ht_Item* maxChainNode = hash_t->buckets[maximumChainLength(hash_t).hashOfMaxChain]; 
-        (void)printf("Maximum Length Chain Node: ");
-        printItemInfo(maxChainNode);
+    // Time Performances and Stats
+    (void)printf("Total time to traverse Hash Table: %f\n", cpu_time_used);
+    ht_print_perfomance_stats(hash_t, argc, argv);
 
-        // Time Performances
-        (void)printf("Total time to traverse Hash Table: %f\n", cpu_time_used);
+    // Saving the Data from the Hash Table in a text file
+    FILE* out = fopen(OUTPUT_PATH, "w");
+    if(!out){
+        (void)printf("Couldn't open the Output file!\n");
+        return -1;
+    }
 
-        printHashTableInfo(hash_t);
+    // Only the unique keys will be in here
+    for(size_t i = 0; i < hash_t->capacity; ++i){
+        Ht_Item* item = hash_t->buckets[i];
+        while(item){
+            const char* key = (char*)item->key;
+            const char* value = (char*)item->val;
+            (void)fprintf(out, "%s %s\n", key, value);
+            item = item->next;
+        }
     }
-    else if(argc > 1 && strcmp(argv[1], "print") == 0){
-        ht_print(hash_t, print_string_string);
-    }
-    
+
+    (void)fclose(out);
     parseFileAndRemoveEntries(hash_t, ENTRIES_PATH);
-
     ht_free(&hash_t);
     return 0;
 }
--- Unit-Testing/lp_test.c
+++ Unit-Testing/lp_test.c
@@ -1,6 +1,43 @@
-#include <stdio.h>
+#include "../src/hash_iterator.h" // include "hash_map.h" too
+#include "../src/hash_map.h"
+#include "../src/hash_utils.h"
+#include <time.h>
 
-int main(){
-    printf("Hello World!!\n");
+#define ENTRIES_PATH "./Data(input)/entries.txt"
+#define OUTPUT_PATH "./Data(output)/lp_out.txt"
+
+int main(int argc, char* argv[]){
+    HashTable* hash_t = ht_create(2, hash_func_str, LINEAR_PROBING);
+    
+    int result = parseFileAndPopulateHashTable(hash_t, ENTRIES_PATH);
+    if(result == -1){
+        return -1;
+    }
+
+     // Saving the Data from the Hash Table in a text file
+    FILE* out = fopen(OUTPUT_PATH, "w");
+    if(!out){
+        (void)printf("Couldn't open the Output file!\n");
+        return -1;
+    }
+
+    // Only the unique keys will be in here
+    for(size_t i = 0; i < hash_t->capacity; ++i){
+        Ht_Item* item = hash_t->buckets[i];
+        while(item){
+            const char* key = (char*)item->key;
+            const char* value = (char*)item->val;
+            (void)fprintf(out, "%s %s\n", key, value);
+            item = item->next;
+        }
+    }
+
+    (void)fclose(out);
+    
+    ht_print_perfomance_stats(hash_t, argc, argv);
+   
+    parseFileAndRemoveEntries(hash_t, ENTRIES_PATH);
+    ht_free(&hash_t);
     return 0;
 }
+
