5dfc2c399157bfd27d46ddc06bb7456d6b67a0c5
calin2244 <bastureacalin@gmail.com>
2023-06-28 00:00:53 +0300

    Updated Testing Env

--- t.c
+++ t.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "./src/hash_map.h"
 #include "./src/hash_utils.h"
 
@@ -9,18 +8,29 @@ typedef struct{
 
 int main(int argc, char* argv[]){
 
-    HashTable* ht = ht_create(4, double_hash_func, CHAINING);
-    testin a = {
-        .a = 2, .b = 4
-    };
-    testin b = {
-        .a = 1, .b = 4
-    };
+    HashTable* ht = ht_create(14, double_hash_func, QUADRATIC_PROBING);
+    // testin a = {
+    //     .a = 2, .b = 4
+    // };
+    // testin b = {
+    //     .a = 1, .b = 4
+    // };
 
-    ht_insert(ht, "Lol", &a, sizeof(testin));
-    ht_insert(ht, "solsd", &b, sizeof(testin));
+    // ht_insert(ht, "Lol", &a, sizeof(testin));
+    // ht_insert(ht, "solsd", &b, sizeof(testin));
 
-    parseFileAndPopulateHashTable(ht, "hashing.txt");
+    ht_insert(ht, "sall", "abc", 4);
+    ht_insert(ht, "sall1", "abc", 4);
+    ht_insert(ht, "sall2", "abc", 4);
+    ht_insert(ht, "sall3", "abc", 4);
+    ht_insert(ht, "Calinn", "abc", 4);
+    ht_modify_item(ht, "sall2", "rege", 5);
+
+    printf("%ld\n", double_hash_func("sall3", 14));
+
+    ht_remove(ht, "sall2");
+
+    // parseFileAndPopulateHashTable(ht, "hashing.txt");
 
     // Silently Ignore
     ht_remove(ht, "ss");
